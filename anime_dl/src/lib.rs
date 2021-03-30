8cad1de83c93495313aa21a3aebc2b13e2bd84d4
Jarmo Riikonen <amatrelan@gmail.com>
2021-03-30 23:07:01 +0300

    Added clippy unwrap_in_result check

--- anime_dl/src/lib.rs
+++ anime_dl/src/lib.rs
@@ -1,17 +1,17 @@
 // TODO: When I have time I will fix these, for now commented out but setted up so I can start when I have time
-// #![warn(clippy::all, clippy::restriction, clippy::pedantic, clippy::nursery, clippy::cargo)]
-// #![deny(clippy::all, clippy::pedantic)]
+#![warn(clippy::all, clippy::restriction, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![deny(clippy::all, clippy::pedantic)]
 
-// #![deny(clippy::unwrap_in_result)]
-// #![deny(clippy::get_unwrap)]
-// #![deny(clippy::cargo_common_metadata)]
-// #![deny(clippy::cast_lossless)]
-// #![deny(clippy::checked_conversions)]
-// #![deny(clippy::clone_on_ref_ptr)]
-// #![deny(clippy::cognitive_complexity)]
-// #![deny(clippy::create_dir)]
-// #![deny(clippy::dbg_macro)]
-// #![deny(clippy::pedantic)]
+#![deny(clippy::unwrap_in_result)]
+#![deny(clippy::get_unwrap)]
+#![deny(clippy::cargo_common_metadata)]
+#![deny(clippy::cast_lossless)]
+#![deny(clippy::checked_conversions)]
+#![deny(clippy::clone_on_ref_ptr)]
+#![deny(clippy::cognitive_complexity)]
+#![deny(clippy::create_dir)]
+#![deny(clippy::dbg_macro)]
+#![deny(clippy::pedantic)]
 
 pub mod error;
 pub mod provider;
